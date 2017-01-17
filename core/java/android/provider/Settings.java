85b92740f60eb5a0d5dea27c51965a3dc7f33e9b
beanstown106 <nbenis106@gmail.com>
2017-01-16 22:27:17 -0500

    One handed mode toggle [1/3]
    Change-Id: Ic4c59a4334600bef86e9e7d28d5f38cd517d11c2
    Signed-off-by: dadi11 <dadi.sikorski@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8676,6 +8676,11 @@ public final class Settings {
          */
         public static final String ALARM_MANAGER_CONSTANTS = "alarm_manager_constants";
 
+        /**
+         * One Handed mode toggle
+         * @hide
+         */
+        public static final String ONE_HANDED_MODE_UI = "one_handed_mode_ui";
         public static final String SINGLE_HAND_MODE = "single_hand_mode";
 
         /**
